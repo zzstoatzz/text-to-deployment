bb35b9fc93c7251d6871006be640d4bd0647c7b1
Nathan Nowack <thrast36@gmail.com>
2023-06-02 16:52:28 -0500

    Add check-eth-price flow.

--- flows/check_eth_price.py
+++ flows/check_eth_price.py
@@ -5,4 +5,4 @@ def check_eth_price():
     import requests
     response = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/spot')
     eth_price = response.json()['data']['amount']
-    return f'ETH price in USD: {eth_price}'
+    return float(eth_price)
