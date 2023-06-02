a7bceb80051109f51c329d9ee082bb69c9d39660
Nathan Nowack <thrast36@gmail.com>
2023-06-02 16:41:51 -0500

    Add fetch_eth_price flow.

--- flows/fetch_eth_price.py
+++ flows/fetch_eth_price.py
@@ -1,8 +1,9 @@
 from prefect import flow
 
 @flow
-def get_eth_price():
+def fetch_eth_price():
     import requests
     response = requests.get('https://api.coinbase.com/v2/prices/ETH-USD/spot')
-    price = response.json()['data']['amount']
-    return float(price)
+    data = response.json()
+    price = data['data']['amount']
+    return price
